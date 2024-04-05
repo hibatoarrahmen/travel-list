c15af168bc1e005b69cab3f947f98744816bcbd6
Hiba Ghrab <ghrabhiba@gmail.com>
2024-04-05 20:27:00 +0100

    first

---
+++ src/Component/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+export default function App() {
+  const [items, setItems] = useState([]);
+  //derived state
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handlelDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleClearList() {
+    const confiremed = window.confirm("ate you sure to selete all items ?");
+    if (confiremed) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handlelDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/Component/Stats.js
@@ -0,0 +1,23 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  const numItem = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItem) * 100);
+  return (
+    //template literal and cond rendering
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "you got evrything ready to go"
+          : `😁You have
+        ${numItem} items on your list ,and you already packed ${numPacked}(
+        ${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
